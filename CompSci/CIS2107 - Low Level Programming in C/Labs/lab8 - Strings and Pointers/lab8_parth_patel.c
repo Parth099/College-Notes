3fd1d8844bcbe07b91fbb72bf6bf9c3cc9c5c9f5
Parth099 <parthnpatel715@gmail.com>
2022-03-31 13:22:06 -0400

    started lab 8

---
+++ CompSci/CIS2107 - Low Level Programming in C/Labs/lab8 - Strings and Pointers/lab8_parth_patel.c
@@ -0,0 +1,176 @@
+/*
+ * Name:	XXX	XXXX 
+ * Section:	XX 
+ * Lab:  	CIS2107_Lab08_Manual 
+ * Goal: 	To design and implement functions taking pointers as arguments 
+			to process characters and strings.
+ */
+
+
+#include <stdio.h>
+#include <memory.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+//functions prototypes
+void upperLower(const char * s);
+int convertStrtoInt(const char *s1, const char *s2, const char *s3, const char *s4);
+float convertStrtoFloat(const char *s1, const char *s2, const char *s3, const char *s4);
+void compareStr(const char *s1, const char *s2);
+void comparePartialStr(const char *s1, const char *s2, int n);
+void randomize(void);
+int tokenizeTelNum(char *num);
+void reverse(char *text);
+int countSubstr (char * line, char * sub);
+int countChar (char * line, char c);
+int countWords(char *string);
+void countAlpha(char *string);
+void startsWithB(char *string[]);
+void endsWithed(char *string[]);
+
+//helpers
+
+int main() {
+
+    //random generator
+    srand(time(NULL));
+
+    //test for upperLower
+    const char test[] = "This iS A Test";
+    upperLower(test);
+
+    //test for convertStrtoInt
+    printf("\n\nThe sum of 4 strings is: %d", convertStrtoInt("3", "4", "5", "6"));
+
+    //test for convertStrtoFloat
+    printf("\n\nThe sum of 4 strings is: %.2f", convertStrtoFloat("3.5", "4.5", "5.5", "6.5"));
+
+    //test for compareStr
+    compareStr("Test1", "Test2");
+
+    //test for comparePartialStr
+    comparePartialStr("Test1", "Test2", 4);
+
+    //test for randomize
+    randomize();
+
+    //test for tokenize number
+    char str[] = "(267) 436-6281";
+    tokenizeTelNum(str);
+
+    //test for reverse
+    char line[] = "Hello world";
+    reverse(line);
+
+    //test for countSubstr
+    char *line1 = "helloworldworld";
+    char *substring = "world";
+    printf("\n\nNumber of Substrings %s inside %s: %d\n", substring, line1, countSubstr(line1, substring));
+
+    //test for countChar
+    char w = 'w';
+    printf("\nNumber of character %c inside %s: %d\n", w, line1, countChar(line1, w));
+
+    //test for countAlpha
+    char str1[] = "Hello it's me.";
+    countAlpha(str1);
+
+    //test for countWords
+    char countstring[] = "hello world!";
+    printf("\n\nNumber of words in string is: %d\n", countWords(countstring));
+
+    //test for startsWithB
+    char *series[] = {"bored", "hello", "Brother", "manual", "bothered"};
+    startsWithB(series);
+
+    //test for endsWithed
+    endsWithed(series);
+
+}
+
+// 1.(Displaying Strings in Uppercase and Lowercase) 
+void upperLower (const char * s) {
+    
+}
+
+// 2.(Converting Strings to Integers for Calculations) 
+int convertStrtoInt(const char *s1, const char *s2, const char *s3, const char *s4) {
+    
+
+}
+
+//3.(Converting Strings to Floating Point for Calculations) 
+float convertStrtoFloat(const char *s1, const char *s2, const char *s3, const char *s4) {
+   
+
+}
+
+//4.(Comparing Strings) 
+void compareStr(const char *s1, const char *s2) {
+    
+
+}
+
+//5.(Comparing Portions of Strings) 
+void comparePartialStr(const char *s1, const char *s2, int n) {
+   
+
+}
+
+//6.(Random Sentences) 
+void randomize(void) {
+   
+   
+}
+
+//7.(Tokenizing Telephone Numbers) 
+int tokenizeTelNum(char *num) {
+  
+  
+}
+
+//8.(Displaying a Sentence with Its Words Reversed) 
+void reverse(char *text) {
+    
+
+}
+
+//9.(Counting the Occurrences of a Substring) 
+int countSubstr (char * line, char * sub) {
+  
+  
+}
+
+//10.(Counting the Occurrences of a Character) 
+int countChar (char *line, char c) {
+  
+  
+}
+
+
+//11.(Counting the Letters of the Alphabet in a String) 
+void countAlpha(char *string) {
+ 
+ 
+}
+
+//12.(Counting the Number of Words in a String) 
+int countWords(char *string) {
+   
+   
+}
+
+//13.(Strings Starting with "b") 
+void startsWithB(char *string[]) {
+
+ 
+
+}
+
+//14.(Strings Ending with "ed") 
+void endsWithed(char *string[]) {
+
+
+}
